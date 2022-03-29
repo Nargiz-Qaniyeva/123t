58f3ea3c7a987d39aa5894240140d68fc506582b
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-29 20:59:24 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Exceptions/CapacityLimitException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Exception
+{
+    internal class CapacityLimitException
+    {
+        public CapacityLimitException(string message) : base(message)
+        {
+            Console.WriteLine("Limiti dolacaq");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Models/Library.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1.Exceptions;
+using ConsoleApp1.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    internal class Library
+    {
+        public string BookLimit { get; set; }
+        Books<int> book = new Books<int>();
+
+        public Library()
+        {
+            this.BookLimit = BookLimit;
+        }
+
+        public void AddBook()
+        {
+            Books.Add(new Book());
+            if (true)
+            {
+                return;
+            }
+            else
+            {
+                throw new CapacityLimitException("Limiti asacaq")
+            }
+        }
+        public object GetBookById(int? id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("Bos deyil");
+            return null;
+        }
+    }
+}
